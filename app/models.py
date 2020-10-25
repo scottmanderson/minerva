5e9d5cbfe46094629d970cf2235a501103ca6d05
Scott Anderson <30956117+scottmanderson@users.noreply.github.com>
2020-10-24 23:24:00 -0500

    database confirmed active with initial models

---
+++ app/models.py
@@ -0,0 +1,30 @@
+from app import app, db, ma
+
+class FinancialObject(db.Model):
+    __tablename__ = 'financial_objects'
+    foid = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True)
+    report_name = db.Column(db.String(100))
+    ticker = db.Column(db.String(20))
+    ts_relationship = db.relationship('TSData',
+                                      backref='FinancialObject',
+                                      cascade='all, delete, delete-orphan',
+                                      lazy='dynamic',
+                                      single_parent=True,
+                                      order_by='asc(TSData.dt)')
+
+    def __init__(self, name, report_name, ticker=None):
+        self.name = name
+        self.report_name = report_name or name
+        self.ticker = ticker
+
+
+class TSData(db.Model):
+    __tablename__ = 'ts_data'
+    tsid = db.Column(db.Integer, primary_key=True)
+    foid = db.Column(db.Integer, db.ForeignKey('financial_objects.foid'))
+    dt = db.Column(db.DateTime)
+    level = db.Column(db.Float)
+    source = db.Column(db.String(100))
+
+

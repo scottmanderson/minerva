35039cb83582e793564d881ecb310ccd5e19e1c6
Scott Anderson <30956117+scottmanderson@users.noreply.github.com>
2020-10-24 23:24:00 -0500

    initial setup before initial database models

---
+++ app/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+from flask_migrate import Migrate
+
+from config import Config
+
+app = Flask(__name__)
+CORS(app)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+migrate = Migrate(app, db)
+
+from app import routes, models
---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+                              "postgresql://localhost/minerva"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True

f16357bf6a7d550dbbabab8544a075458df41660
Scott Anderson <30956117+scottmanderson@users.noreply.github.com>
2020-10-25 11:29:03 -0500

    added flask shell context

--- minerva.py
+++ minerva.py
@@ -1,1 +1,15 @@
 from app import app, db
+from app.models import FinancialObject, TSData
+
+import numpy as np
+import pandas as pd
+
+@app.shell_context_processor
+def make_shell_context():
+    return {
+        'db': db,
+        'FinancialObject': FinancialObject,
+        'TSData': TSData,
+        'np': np,
+        'pd': pd
+    }

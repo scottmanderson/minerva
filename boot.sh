03c2570a7c13b9d041b5091684237b8cbce8d9f4
Scott Anderson <30956117+scottmanderson@users.noreply.github.com>
2020-11-08 15:43:52 -0600

    startup script for the production (gunicorn) added

--- boot.sh
+++ boot.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-while true; do
+#while true; do
     flask db upgrade
-    if [[ "$?" == "0" ]]; then
-        break
-    fi
-    echo Upgrade command failed, retrying in 15 secs...
-    sleep 15
-done
+#    if [[ "$?" == "0" ]]; then
+#        break
+#    fi
+#    echo Upgrade command failed, retrying in 5 secs...
+#    sleep 5
+#done
 flask translate compile
 exec gunicorn -b :5000 --access-logfile - --error-logfile - minerva:app

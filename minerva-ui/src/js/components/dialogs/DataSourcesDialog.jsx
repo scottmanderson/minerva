7810281f6dacaf3b601ae6ed26f116dc8dd3f705
Scott Anderson <30956117+scottmanderson@users.noreply.github.com>
2020-11-25 22:22:09 -0600

    form styling changes

--- minerva-ui/src/js/components/dialogs/DataSourcesDialog.jsx
+++ minerva-ui/src/js/components/dialogs/DataSourcesDialog.jsx
@@ -28,22 +28,34 @@ const DataSourcesDialog = (props) => {
             <Grid
               container
               direction="row"
-              justify="flex-start"
-              alignItems="center"
+              justify="space-evenly"
+              alignItems="baseline"
             >
+              <Grid container item xs={12}>
+                <Grid item xs={4}>
+                  Data Source Name
+                </Grid>
+                <Grid item xs={4}>
+                  Source Rank
+                </Grid>
+                <Grid item xs={4}>
+                  API Key
+                </Grid>
+              </Grid>
+              <br />
               {props.dataSources.map((el) => (
                 <>
                   <Grid container item xs={12}>
-                    <Grid item xs={2} spacing={3}>
+                    <Grid item xs={4} spacing={3}>
                       <TextField key={"dsl" + el} defaultValue={el.name} />
                     </Grid>
-                    <Grid item xs={2} spacing={3}>
+                    <Grid item xs={4} spacing={3}>
                       <TextField
                         key={"dsr" + el}
                         defaultValue={el.hierarchy_rank}
                       />
                     </Grid>
-                    <Grid item xs={2} spacing={3}>
+                    <Grid item xs={4} spacing={3}>
                       <TextField key={"dsk" + el} defaultValue={el.api_key} />
                     </Grid>
                   </Grid>

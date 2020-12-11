a5e1c4ad984a99ecb48ecc90cac3dcdac136d568
Scott Anderson <30956117+scottmanderson@users.noreply.github.com>
2020-12-10 18:09:16 -0600

    settings saga mounted from rootSaga export

--- minerva-ui/src/js/redux/sagas/rootSaga.js
+++ minerva-ui/src/js/redux/sagas/rootSaga.js
@@ -5,6 +5,7 @@ import getStat from "./getStatSaga";
 import getDefaultBenchmarkStat from "./getDefaultBenchmarkStatSaga";
 import getDataSources from "./getDataSourcesSaga";
 import getDataSourcePolls from "./getDataSourcePollsSaga";
+import getSettingsSaga from "./getSettingsSaga";
 
 export default function* rootSaga() {
   yield all([
@@ -13,5 +14,6 @@ export default function* rootSaga() {
     getDefaultBenchmarkStat(),
     getDataSources(),
     getDataSourcePolls(),
+    getSettingsSaga(),
   ]);
 }

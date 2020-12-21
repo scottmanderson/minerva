69e807e7a5f78e5dfe3d172ecb2fb76662b20ac9
Scott Anderson <30956117+scottmanderson@users.noreply.github.com>
2020-12-21 12:39:33 -0600

    adding duplicate application variable name to make app discoverable by eb

--- minerva.py
+++ minerva.py
@@ -1,4 +1,4 @@
-from app import app, db
+from app import app, application, db
 from app.models import FinancialObject, TSData
 
 import numpy as np
@@ -8,9 +8,9 @@ import pandas as pd
 @app.shell_context_processor
 def make_shell_context():
     return {
-        'db': db,
-        'FinancialObject': FinancialObject,
-        'TSData': TSData,
-        'np': np,
-        'pd': pd
+        "db": db,
+        "FinancialObject": FinancialObject,
+        "TSData": TSData,
+        "np": np,
+        "pd": pd,
     }
